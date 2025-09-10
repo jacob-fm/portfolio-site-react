5482b43d2d0d52f87b1045c610b3661b2f0f4635
JacobJFM <jacobfeitmann@gmail.com>
2025-09-10 04:07:47 -0400

    start moving bopmatch content

--- src/routes/Bopmatch.jsx
+++ src/routes/Bopmatch.jsx
@@ -1,3 +1,33 @@
+import LinkButton from "../components/LinkButton";
 export default function Bopmatch() {
-  return <p>Hello!</p>;
+  return (
+    <>
+      <h1 className="text-2xl text-center">BopMatch</h1>
+      <hr className="m-6"></hr>
+      <LinkButton route="https://bopmatch.com">Play BopMatch!</LinkButton>
+      <p>
+        In BopMatch, players are shown a <i>target artist</i>{" "}
+        and are tasked with choosing another musician or band who they think
+        will have equivalent <i>popularity.</i>{" "}
+        The closer the two are in popularity, the higher the score.
+      </p>
+      <p>
+        <i>Popularity</i> is a statistic available via the{" "}
+        <a href="https://developer.spotify.com/documentation/web-api">
+          Spotify Web API
+        </a>, which I also pull from to allow players to search for and select
+        any musician on Spotify.
+      </p>
+      <p>
+        BopMatch was built using React and Vite on the front-end, and Supabase
+        for the backend.
+      </p>
+      <figure>
+        <img
+          src="/media/bopmatch/homepage.png"
+          alt="Screenshot of BopMatch"
+        />
+      </figure>
+    </>
+  );
 }

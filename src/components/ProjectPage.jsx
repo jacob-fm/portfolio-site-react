cfa0644991c7a067ce3672abb079358cad84a62e
JacobJFM <jacobfeitmann@gmail.com>
2025-09-10 05:35:55 -0400

    home button on project pages

--- src/components/ProjectPage.jsx
+++ src/components/ProjectPage.jsx
@@ -1,9 +1,18 @@
+import { Link } from "react-router-dom";
+
 export default function ProjectPage({ title, children }) {
   return (
     <>
       <h1 className="text-2xl text-center mt-6">{title}</h1>
       <hr className="m-6"></hr>
       <div className="space-y-2">{children}</div>
+      <Link
+        to="/"
+        className="w-min mx-auto flex items-center justify-center mt-5 gap-1.5 py-2 px-4 text-lg text-primary border border-primary rounded-lg hover:text-pink-500 hover:border-pink-500 active:text-blue-700 active:border-blue-700"
+      >
+        <i className="fa-solid fa-arrow-left"></i>
+        <span>Home</span>
+      </Link>
     </>
   );
 }

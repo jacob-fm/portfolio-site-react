608cf7316ccbe730113f2d732282f81c6244c5c3
JacobJFM <jacobfeitmann@gmail.com>
2025-09-10 04:07:35 -0400

    LinkButton component

---
+++ src/components/LinkButton.jsx
@@ -0,0 +1,10 @@
+export default function LinkButton({ children, route }) {
+  return (
+    <a
+      className="flex w-40 p-2 mx-auto my-5 justify-center rounded-lg bg-linear-to-r from-primary to-primary-dark text-white shadow-md transform transition-all duration-200 hover:-translate-y-0.5 hover:-translate-x-0.5 hover:shadow-lg hover:from-pink-700 hover:to-pink-900 active:bg-amber-200"
+      href={route}
+    >
+      {children}
+    </a>
+  );
+}

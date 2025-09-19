e140b43876c71fe93062f1b6fee1a98f52580ba6
JacobJFM <jacobfeitmann@gmail.com>
2025-09-19 13:04:14 -0400

    some content for nigunnim

--- src/routes/Nigunnim.jsx
+++ src/routes/Nigunnim.jsx
@@ -3,7 +3,40 @@ import ProjectPage from "../components/ProjectPage";
 export default function Nigunnim() {
   return (
     <ProjectPage title="Nigunnim">
-      <p>helloooo</p>
+      <p>
+        "Nigunnim" was an artwork I created for the Blanksteen Fellowship, at
+        the Joseph Slifka Center for Jewish Life at Yale in 2023. I, along with
+        the rest of the cohort, were given the phrase "How does speech become
+        violence?" as a prompt for our work. I was inspired to use a mixture of
+        audio and video synthesis tools to explore wordless tunes, similar to
+        the "nigunnim" I grew up with.
+      </p>
+      <p>
+        From my artist statement:
+      </p>
+      <blockquote className="bg-bg-dark">
+        <p>
+          When approaching the themes of speech and violence, I dug in to find
+          something that spoke to me in relation to what l've made in the past.
+          I thought about violence due to lack of speech, and the frustration
+          and anger that can arise when words fail. I thought back to my days in
+          summer camp; in particular, I remembered debates with a friend on
+          whether the nigunnim (tunes) we sang on Shabbat were better with or
+          without lyrics.
+        </p>
+        <br />
+        <p>
+          My friend argued that when we sang the tunes without words, we were
+          tapping into something deeper and truer than what our words could say.
+          At the time I thought it was nonsensical and silly. Looking back, I'm
+          not so sure. This piece was an opportunity to take what l've learned
+          about music production to explore and compose nigunnim that make use
+          of modern electronic instrumentation while incorporating the
+          meditative, trance-like qualities of a wordless nigun. I used the
+          accompanying visuals to transform a purely auditory experience into an
+          even more immersive atmosphere.
+        </p>
+      </blockquote>
     </ProjectPage>
   );
 }

9d1fe6b36dddcaf19e95650e675a82e744b0a692
JacobJFM <jacobfeitmann@gmail.com>
2025-12-01 23:17:56 -0500

    alien egg update

--- src/routes/AlienEgg.jsx
+++ src/routes/AlienEgg.jsx
@@ -2,10 +2,21 @@ import ProjectPage from "../components/ProjectPage";
 import LinkButton from "../components/LinkButton";
 export default function AlienEgg() {
   return (
-    <ProjectPage title="&quot;Alien Egg&quot; Sculpture">
+    <ProjectPage title='"Alien Egg" Sculpture'>
       <LinkButton route="https://github.com/material-kish/arduino_gold">
         <i className="fa-brands fa-github"></i>Github Repo
       </LinkButton>
+      <img
+        src="/media/alien_egg/close_up.jpg"
+        alt="Close-up shot of Alien Egg sculpture"
+      />
+      <div className="space-y-2 flex flex-col ">
+        <video preload="none" controls loop muted className="aspect-video">
+          <source src="/media/alien_egg/demonstration.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        <figcaption>Video of the sculpture in action</figcaption>
+      </div>
       <p>
         I built this sculpture for an exhibition of artwork by Yale college
         seniors. I had no prior experience with any of the materials I used - I
@@ -33,9 +44,7 @@ export default function AlienEgg() {
           src="/media/alien_egg/wide_angle.jpg"
           alt="Wide angle shot of the Alien Egg sculpture in the gallery"
         />
-        <figcaption>
-          The egg sitting in its enclave
-        </figcaption>
+        <figcaption>The egg sitting in its enclave</figcaption>
       </figure>
       <p>
         I placed the sculpture on a pedestal, tucked away under the staircase in
@@ -45,19 +54,17 @@ export default function AlienEgg() {
         that as people approached the sculpture, the LED strips would shine
         brighter, and as they moved away, the lights would dim.
       </p>
-      <img
-        src="/media/alien_egg/close_up.jpg"
-        alt="Close-up shot of Alien Egg sculpture"
-      />
-      <div className="space-y-2 flex flex-col ">
-        <video preload="none" controls loop muted className="aspect-video">
-          <source src="/media/alien_egg/demonstration.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-        <figcaption>
-          Video of the sculpture in action
-        </figcaption>
-      </div>
+      <p>
+        To me, the sculpture was some sort of creature, and some people told me
+        they felt that way too. But I deliberately refrained from any gestures
+        that would suggest this too explicitly, like giving the sculpture a name
+        or a face. I wanted to see how people would approach it and
+        conceptualize it without that sort of hand-holding.
+      </p>
+      <p>
+        I was told it looked like a brain, or a rock, or Wall-E, all of which I
+        agree with.
+      </p>
     </ProjectPage>
   );
 }

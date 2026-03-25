5259de9deb97ff6dcbd79b9e22989c3a37b74253
JacobJFM <jacobfeitmann@gmail.com>
2026-03-24 22:12:15 -0400

    move back button on project pages to top

--- src/components/ProjectPage.jsx
+++ src/components/ProjectPage.jsx
@@ -7,16 +7,16 @@ export default function ProjectPage({ title, children }) {
 
   return (
     <div className="project-page max-w-200 mx-auto">
+      <Link
+        to="/"
+        className="fixed top-4 left-4 flex items-center gap-1.5 py-2 px-4 text-lg text-primary border border-primary rounded-lg hover:text-pink-500 hover:border-pink-500 active:text-blue-700 active:border-blue-700 bg-bg z-50"
+      >
+        <i className="fa-solid fa-arrow-left"></i>
+        <span className="hidden sm:inline">Home</span>
+      </Link>
       <h1 className="text-2xl text-center mt-6">{title}</h1>
       <hr className="m-6"></hr>
       <div className="space-y-4">{children}</div>
-      <Link
-        to="/"
-        className="w-min mx-auto flex items-center justify-center mt-5 gap-1.5 py-2 px-4 text-lg text-primary border border-primary rounded-lg hover:text-pink-500 hover:border-pink-500 active:text-blue-700 active:border-blue-700"
-      >
-        <i className="fa-solid fa-arrow-left"></i>
-        <span>Home</span>
-      </Link>
     </div>
   );
 }

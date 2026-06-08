cd9424b9e256416215549e11bf00ffe2fe8f7e98
JacobJFM <jacobfeitmann@gmail.com>
2026-06-08 12:15:36 -0400

    basic masonry layout for pointcloud parties page

--- src/routes/projects/PointCloudParties.jsx
+++ src/routes/projects/PointCloudParties.jsx
@@ -1,5 +1,26 @@
 import ProjectPage from "../../components/ProjectPage";
 
 export default function PointCloudParties() {
-  return <ProjectPage title="Point Cloud Parties"></ProjectPage>;
+  const images = [
+    "/media/pointcloud/chris_fisheye_1.jpg",
+    "/media/pointcloud/crowd_shot_hero.webp",
+    "/media/pointcloud/green_fisheye.jpg",
+    "/media/pointcloud/green_screen_close_up.jpg",
+    "/media/pointcloud/ian_spinning.jpg",
+    "/media/pointcloud/jacob_w_mic.jpg",
+    "/media/pointcloud/pink_wall.jpg",
+  ];
+  return (
+    <ProjectPage title="Point Cloud Parties">
+      <div className="mt-6 mx-auto px-6 columns-[300px] gap-2">
+        {images.map((image) => (
+          <img
+            src={image}
+            key={image}
+            className="w-full mb-2 break-inside-avoid"
+          />
+        ))}
+      </div>
+    </ProjectPage>
+  );
 }
